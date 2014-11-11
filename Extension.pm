974edb26b548a4fe57a2936b2fa4d4bab7b69701
Pami Ketolainen <pami.ketolainen@jolla.com>
2014-11-11 20:54:50 +0200

    Add selectively updated time stamp for activity

--- Extension.pm
+++ Extension.pm
@@ -25,7 +25,7 @@ sub buglist_columns {
     my ($self, $args) = @_;
     $args->{columns}->{days_since_activity} = {
         'title' => 'Since activity',
-        'name' => 'TIMESTAMPDIFF(SECOND, bugs.delta_ts, NOW()) / 86400'
+        'name' => 'TIMESTAMPDIFF(SECOND, bugs.ag_activity_ts, NOW()) / 86400'
     };
 }
 
@@ -41,6 +41,48 @@ sub config_add_panels {
     my $modules = $args->{panel_modules};
     $modules->{ActivityGraph} = "Bugzilla::Extension::ActivityGraph::Params";
 }
+
+sub install_update_db {
+    my ($self, $args) = @_;
+    my $dbh = Bugzilla->dbh;
+    my $activity_col = $dbh->bz_column_info('bugs', 'ag_activity_ts');
+    if (!defined $activity_col) {
+        $dbh->bz_add_column('bugs', 'ag_activity_ts', {TYPE => 'DATETIME'});
+        $dbh->do('UPDATE bugs SET ag_activity_ts = delta_ts');
+    }
+
+}
+
+sub object_columns {
+    my ($self, $args) = @_;
+    if ($args->{class}->isa('Bugzilla::Bug')) {
+        push(@{$args->{columns}}, 'ag_activity_ts');
+    }
+}
+
+sub bug_end_of_create {
+    my ($self, $args) = @_;
+    my ($bug, $timestamp) = @$args{qw(bug timestamp)};
+    Bugzilla->dbh->do("UPDATE bugs SET ag_activity_ts = ? WHERE bug_id = ?",
+                        undef, $timestamp, $bug->id);
+}
+
+sub bug_end_of_update {
+    my ($self, $args) = @_;
+    my ($bug, $changes, $timestamp) = @$args{qw(bug changes timestamp)};
+    # TODO: Make the significant fields configurable
+    if ($bug->{added_comments}
+        || defined $changes->{bug_status}
+        || defined $changes->{resolution}
+        || defined $changes->{work_time}
+        || defined $changes->{see_also})
+    {
+        Bugzilla->dbh->do("UPDATE bugs SET ag_activity_ts = ? WHERE bug_id = ?",
+                            undef, $timestamp, $bug->id);
+    }
+}
+
+
 
 sub template_before_process {
     my ($self, $params) = @_;
@@ -66,13 +108,15 @@ use Bugzilla::WebService::Bug ();
 }
 
 BEGIN {
+    *Bugzilla::Bug::activity_ts = sub { return $_[0]->{ag_activity_ts}; };
+
     *Bugzilla::Bug::days_since_activity = sub {
         my $self = shift;
         unless (defined $self->{days_since_activity}) {
             require DateTime;
+            my $ts = $self->activity_ts || $self->delta_ts;
             $self->{days_since_activity} =
-                (DateTime->now()->epoch - datetime_from($self->delta_ts)->epoch) / 86400;
-
+                (DateTime->now()->epoch - datetime_from($ts)->epoch) / 86400;
         }
         return $self->{days_since_activity};
     };
